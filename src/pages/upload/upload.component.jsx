25c7100f4f3ef6b2141831784f3c24d1baf7079d
Maciej <ostromaciej@gmail.com>
2020-08-17 10:29:20 +0200

    set up state management for the post form upload

--- src/pages/upload/upload.component.jsx
+++ src/pages/upload/upload.component.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./upload.styles.scss";
 
 const Upload = () => {
+  const [caption, setCaption] = useState("Fortell litt om leiligheten");
+  const [renter, setRenter] = useState("Utleier");
+  const [adress, setAdress] = useState("Adresse");
+  const [city, setCity] = useState("By");
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    e.target.files[0] ? setImage(e.target.files[0]) : console.log("e");
+  };
+
+  const handleUpload = () => {};
+
   return (
     <div className="upload">
       <h2 className="upload-title">Last opp din drittleilighet!</h2>
@@ -13,17 +25,32 @@ const Upload = () => {
           type="text"
           placeholder="Utleier"
           className="upload-form-input"
+          value={renter}
+          onChange={(e) => setRenter(e.target.value)}
         />
         <input
           type="text"
           placeholder="Adresse"
           className="upload-form-input"
+          value={adress}
+          onChange={(e) => setAdress(e.target.value)}
         />
-        <input type="text" placeholder="By" className="upload-form-input" />
-        <textarea className="upload-form-textarea">
-          Fortell litt om leiligheten.
-        </textarea>
-        <button className="upload-form-button">Last Opp</button>
+        <input
+          type="text"
+          placeholder="By"
+          className="upload-form-input"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <textarea
+          className="upload-form-textarea"
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
+        />
+        <input type="file" />
+        <button className="upload-form-button" onChange={handleImageChange}>
+          Last Opp
+        </button>
       </form>
     </div>
   );
